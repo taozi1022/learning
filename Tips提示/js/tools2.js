cff31ae8ea3ec22685ffca9dbc9734d7311c3bb3
taozi1022 <taozi.1022@outlook.com>
2016-09-04 21:03:39 +0800

    鼠标移入关键字,显示关键字的详细内容

---
+++ Tips提示/js/tools2.js
@@ -0,0 +1,65 @@
+var tools = {
+	getStyle:function (obj,attr){
+		if( obj.currentStyle ){
+			return obj.currentStyle[attr];
+		}else{
+			return getComputedStyle(obj)[attr];
+		}	
+	},
+	hasClass:function (element,classNames) {
+		var classAll = 	element.className.split(" ");  //"blue red"
+		for( var i = 0; i < classAll.length; i++ ){
+			if( classAll[i] === classNames ){
+				return true;
+			}
+		}
+		return false;
+	},
+	removeClass:function (element,classNames){
+		if( tools.hasClass(element,classNames) ){  //如果有这个class，就删除
+			var classAll = element.className.split(" "); // ["blue","red"]
+			for( var i = 0; i < classAll.length; i++ ){
+				if( classAll[i] === classNames ){
+					classAll.splice(i,1);
+					i--;
+				}
+			}
+			//如果这个数组为kong，那么就删除标签上的class这个属性
+			if( classAll.length === 0 ){
+				element.removeAttribute("class");
+			}else{
+				element.className = classAll.join(" ");
+			}
+		}
+	},
+	addClass:function (element,classNames){
+		var classAll = element.className;  // ""    "blue"
+		if( !tools.hasClass(element,classNames) ){
+			classAll += " " + classNames;
+		}
+		element.className = classAll.trim();
+	},
+	toggleClass:function (element,classNames){
+		if( tools.hasClass(element,classNames) ){
+			tools.removeClass(element,classNames);
+			return false;
+		}else{
+			tools.addClass(element,classNames);
+			return true;
+		}	
+	},
+	insertAfter:function (newElement,targetElement){
+		//先找到父节点
+		var parent = targetElement.parentNode;
+		//插入到targetElement下一个兄弟节点的前面
+		if( targetElement === parent.lastElementChild ){
+			parent.appendChild(newElement)
+		}else{
+			parent.insertBefore(newElement,targetElement.nextElementSibling);
+		}
+
+	},
+	getRect:function (obj){
+		return obj.getBoundingClientRect();
+	}
+}
